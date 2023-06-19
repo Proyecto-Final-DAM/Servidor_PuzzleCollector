d98583a818447c2b78d9579e5c09943bfe02c692
Juan Antonio Nicolas <janicolasinformatica@gmail.com>
2023-06-19 09:40:12 +0200

    Added Notes Into Collection Commit

--- src/main/java/com/janicolas/CollectorServer/repository/CollectionRepository.java
+++ src/main/java/com/janicolas/CollectorServer/repository/CollectionRepository.java
@@ -5,8 +5,11 @@ import com.janicolas.CollectorServer.entity.CollectionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CollectionRepository extends JpaRepository<Collection, CollectionId> {
     List<Collection> findByUserId(Long id);
+
+    Optional<Collection> findByUserIdAndPuzzleId(Long user_id, Long puzzle_id);
     Boolean existsByPuzzleIdAndUserId(Long puzzle_id, Long user_id);
 }
